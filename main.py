1a553b31c26dda0a008c51a13a6bbcf9ce8acea6
Evan Duffield <eduffield82@gmail.com>
2016-09-21 08:11:27 -0700

    main.py
    init

---
+++ main.py
@@ -0,0 +1,39 @@
+
+class DashObject():
+class DashDriveObject(DashObject):
+class GovernanceObject(DashObject):
+class CollateralObject(TX):
+class CreditDebitObject(TX):
+	def __init__(self):
+	def creditsleft():
+	def addused(DashObject):
+
+class Node():
+	def __init__(self, a, b):
+		pass
+
+class Pool():
+
+	def __init__(self, a, b):
+		pages = []
+
+	def addgovobj():
+		pass
+
+	def process_pages():
+		pass
+
+class User(DashObject):
+	def __init__
+
+	def random_usage():
+		#try saving, etc
+
+
+p = Pool()
+
+# create 4000 nodes
+for x in range(1, 4000):
+	n.Node()
+	p.append(n)
+
